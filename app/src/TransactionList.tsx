ebe7241381f053c6a43cedb224e4e582276a1bfa
SeanVenz <seanvenz.quijano@cit.edu>
2023-11-10 14:11:15 +0800

    tae

--- app/src/TransactionList.tsx
+++ app/src/TransactionList.tsx
@@ -1,61 +1,53 @@
 import React, { useEffect, useState } from 'react';
 import { useConnection } from '@solana/wallet-adapter-react';
-import { PublicKey, ConfirmedSignatureInfo, ParsedInstruction, ParsedTransaction } from '@solana/web3.js';
+import { PublicKey, ParsedTransaction } from '@solana/web3.js';
 
-const TransactionsList = () => {
+const TransactionsList: React.FC = () => {
   const { connection } = useConnection();
   const [transactions, setTransactions] = useState<ParsedTransaction[]>([]);
-  const programId = new PublicKey('2cE9Naq1VKCKTt2DvYoLfR1U6jS5tBpa8PFckbj8aB3p'); // Replace with actual program ID
+  const programId = new PublicKey('2cE9Naq1VKCKTt2DvYoLfR1U6jS5tBpa8PFckbj8aB3p');
 
   useEffect(() => {
     const fetchTransactions = async () => {
-      // Get the signatures of all transactions involving the program ID
       const signatures = await connection.getSignaturesForAddress(programId);
-
-      // Fetch and parse detailed transaction information for each signature
       const detailedTransactions = await Promise.all(
         signatures.map(async (signatureInfo) => {
-          const transaction = await connection.getParsedConfirmedTransaction(signatureInfo.signature);
-          return transaction;
+          return connection.getParsedConfirmedTransaction(signatureInfo.signature);
         })
       );
-
       setTransactions(detailedTransactions.filter((tx): tx is ParsedTransaction => tx !== null));
     };
 
     fetchTransactions();
   }, [connection, programId]);
 
-  // Function to render the transaction information
-  const renderTransactionInfo = (transaction: ParsedTransaction) => {
-    // Loop through all the instructions and find ones that are relevant to your program
-    const businessInstructions = transaction.transaction.message.instructions.filter(
-      (instr): instr is ParsedInstruction => 
-        'parsed' in instr && 
-        instr.programId.equals(programId) &&
-        instr.parsed.info.name === 'createBusiness' // Adjust if necessary to match your actual instruction name
-    );
+  const renderTransactionInfo = (instruction: any, index: number) => {
+    console.log('Parsed Instruction:', instruction); // Add this to check the structure of the parsed instruction
 
-    // Map the instructions to JSX elements
-    return businessInstructions.map((instr, index) => (
-      <div key={index}>
-        <p>Business Name: {instr.parsed.info.name}</p>
-        {/* Render other business information here */}
-      </div>
-    ));
+    if (instruction.parsed && instruction.parsed.type === 'createBusiness') {
+      const info = instruction.parsed.info;
+      return (
+        <div key={index}>
+          <p>Business Name: {info.name}</p>
+          <p>Business Type: {info.businessType}</p>
+          <p>Business Address: {info.address}</p>
+        </div>
+      );
+    }
+    return null;
   };
 
   return (
     <div>
       <h2>Transactions</h2>
-      <ul>
-        {transactions.map((tx, index) => (
-          <li key={index}>
-            {tx.transaction.signatures[0]}
-            {renderTransactionInfo(tx)}
-          </li>
-        ))}
-      </ul>
+      {transactions.map((tx, index) => (
+        <div key={index}>
+          <p>Transaction Signature: {tx.transaction.signatures[0]}</p>
+          {tx.transaction.message.instructions
+            .filter(instr => instr.programId.equals(programId))
+            .map((instr, idx) => renderTransactionInfo(instr, idx))}
+        </div>
+      ))}
     </div>
   );
 };
